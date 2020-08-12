66bab6414ff197f2923f629271c395655ecc5d13
ryanl <rleung1004@gmail.com>
2020-08-12 15:49:31 -0700

    Implemented place method

--- GUI.py
+++ GUI.py
@@ -18,7 +18,15 @@ class Grid:
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
 
-    def __init__(self, rows, cols, width, height):
+    def __init__(self, rows: int, cols: int, width: int, height: int):
+        """
+        Construct a grid for sudoku.
+
+        :param rows: an integer indicating how many rows the grid will have
+        :param cols: an integer indicating how many columns the grid will have
+        :param width: an integer indicating the width of the model
+        :param height: an integer indicating the height of the model
+        """
         self.rows = rows
         self.cols = cols
         self.boxes = [[Box(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
@@ -32,10 +40,25 @@ class Grid:
         """ Update the Grid into new state """
         self.model = [[self.boxes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
+    def place(self, val: int) -> bool:
+        """
+        Place a number into a box
 
-    def place(self, val):
-        """ Place a number into a box """
+        :param val: an integer 1 to 9
+        :return: a boolean True or False if guess is correct or not
+        """
+        row, col = self.selected
+        if self.boxes[row][col].value == 0:
+            self.boxes[row][col].set(val)
+            self.update_model()
 
+            if sudoku.valid(self.model, val, (row, col)) and sudoku.solve(self.model):
+                return True
+            else:
+                self.boxes[row][col].set(0)
+                self.boxes[row][col].set_temp(0)
+                self.update_model()
+                return False
 
     def sketch(self, val):
         """ Sketch a placeholder onto a box """
