a990e25fabba0d5e975c61a0090ab3a3cc46f1f9
ryanl <rleung1004@gmail.com>
2020-08-13 17:05:01 -0700

    GUI now generates a new board everytime a new game instance is made

--- GUI.py
+++ GUI.py
@@ -235,17 +235,8 @@ def redraw_window(win, board, play_time, strikes):
 def main():
     win = pygame.display.set_mode((540, 600))
     pygame.display.set_caption("Sudoku")
-    board = [
-        [7, 8, 0, 4, 0, 0, 1, 2, 0],
-        [6, 0, 0, 0, 7, 5, 0, 0, 9],
-        [0, 0, 0, 6, 0, 1, 0, 7, 8],
-        [0, 0, 7, 0, 4, 0, 2, 6, 0],
-        [0, 0, 1, 0, 5, 0, 9, 3, 0],
-        [9, 0, 4, 0, 6, 0, 0, 0, 5],
-        [0, 7, 0, 3, 0, 0, 0, 1, 2],
-        [1, 2, 0, 0, 0, 7, 4, 0, 0],
-        [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    ]
+    puzzle = sudoku.Sudoku()
+    board = puzzle.board
     game_board = Grid(9, 9, 540, 540, board, win)
     key = None
     run = True
